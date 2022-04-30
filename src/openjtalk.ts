e0c7bdd61f728814aa7fe0b323b9ffba8c9bd9d0
Soukun <soushi_mizuno@outlook.jp>
2022-04-30 14:21:59 +0900

    変更

--- src/openjtalk.ts
+++ src/openjtalk.ts
@@ -3,70 +3,7 @@ import path from 'path'
 const uuid = require('uuid-v4')
 import {VoiceOption, OpenJTalkArgument } from "./voiceOption"
 import { Command, execCommands } from './command'
-import { ResultAudio } from "./resultAudio"
 import type { Readable } from 'stream'
-
-export function runOpenJTalk (str: string, option: VoiceOption | OpenJTalkArgument): Promise<ResultAudio>{
-	return new Promise((resolve, reject)=>{
-		const mes = [
-            '',
-            '-------------------------------------------------------------------------',
-            '[!] This is not an error.',
-            '-------------------------------------------------------------------------',
-            '    The function "runOpenJTalk" has been deprecated since version 1.1.0.',
-            '    Please use the OpenJTalk class instead.',
-            '-------------------------------------------------------------------------',
-            ''
-        ]
-        console.log(mes.join('\n'))
-        try {
-            const tempDirPath = path.join(__dirname, '../temp/')
-            if (!fs.existsSync(tempDirPath)) {
-                fs.mkdirSync(tempDirPath)
-            }
-	
-            const command = new Command('open_jtalk')
-			const options = ((option: VoiceOption | OpenJTalkArgument): OpenJTalkArgument => {
-                if(option instanceof VoiceOption) return option.values
-                return option
-            })(option)
-            
-            const wavFileName = ((ow?: string)=>{
-                if(ow) return ow
-                return path.join(__dirname, '../temp/' + uuid() + '.wav')
-            })(options.ow)
-            const txtFileName = path.join(__dirname, '../temp/' + uuid() + '.txt')
-
-            command.setArg('a',options.a)
-            command.setArg('b',options.b)
-            command.setArg('fm',options.fm)
-            command.setArg('jf',options.jf)
-            command.setArg('jm',options.jm)
-            command.setArg('m',options.m)
-            command.setArg('ot',options.ot)
-            command.setArg('ow',wavFileName)
-            command.setArg('p',options.p)
-            command.setArg('r',options.r)
-            command.setArg('s',options.s)
-            command.setArg('u',options.u)
-            command.setArg('x',options.x)
-            command.setArg('z',options.z)
-			command.setValue(txtFileName)
-	
-			fs.writeFileSync(txtFileName, str)
-
-            command.exec()
-            .then(()=>{
-				resolve(new ResultAudio(wavFileName))
-				fs.unlinkSync(txtFileName)
-            })
-            .catch(reject)
-
-		} catch (error) {
-			reject(error)
-		}
-	})
-}
 
 export class OpenJTalk {
     constructor(
@@ -97,6 +34,10 @@ export class OpenJTalk {
             const play = getPlayCommand()
             if(!play){
                 return reject("Playback on this OS is not supported.")
+            }
+            const tempDir = path.join(__dirname, '../temp')
+            if (!fs.existsSync(tempDir)){
+                fs.mkdirSync(tempDir)
             }
             const wavPath = path.join(__dirname, '../temp/' + uuid() + '.wav')
             this.outFile(str, config, wavPath)
