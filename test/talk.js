7e4e36628864ac9c2826ba2346c4a128d8cb1ac0
Soukun <soushi_mizuno@outlook.jp>
2022-05-22 20:23:17 +0900

    変更

--- test/talk.js
+++ test/talk.js
@@ -1,17 +1,19 @@
 const { OpenJTalk, VoiceOption } = require('../dist/index')
 
+//インスタンスの作成
+const openjtalk = new OpenJTalk()
+
 //生成する音声の原稿
 const str = "こんにちわ、これはOpenJTalkによって生成された音声です。"
 
 //生成する音声の設定
 const option = new VoiceOption({
-    speaker : "mei_normal", // 必須オプション
-    pitch : 200,            // 省略可能
-    speed : 1               // 省略可能
+    speaker : "mei_normal", // 話者         :必須オプション
+    pitch : 0,              // ピッチシフト  :省略可能
+    speed : 1,              // 話速         :省略可能
+    allPath: 0.55,          // 声質         :省略可能
+    intonation: 1,          // 抑揚         :省略可能
 })
-
-//インスタンスの作成
-const openjtalk = new OpenJTalk()
 
 //音声を生成し再生
 openjtalk.talk(str, option)
