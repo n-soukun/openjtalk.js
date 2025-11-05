f4000191dc8018b586fb4cb851be24287708df9b
Soukun <73824766+n-soukun@users.noreply.github.com>
2025-11-05 18:37:19 +0900

    新しいバイナリーに対応
    ダウンロードしたバイナリーを使用するように変更

--- src/command.ts
+++ src/command.ts
@@ -1,77 +1,100 @@
-import { ChildProcessWithoutNullStreams, execFile, spawn } from "child_process";
-import { Readable } from "stream";
+import { ChildProcessWithoutNullStreams, execFile, spawn } from 'child_process';
+import { Readable } from 'stream';
 
 export class Command {
     constructor(
         public name: string,
-        private args?: {[key: string]: string;},
-        private value?: string 
-    ){}
+        private args?: { [key: string]: string },
+        private value?: string
+    ) {}
     setArg(key: string, value: any): boolean {
-        if(!this.args) this.args = {}
-        if(value){
-            this.args[key] = String(value)
-            return true
+        if (!this.args) this.args = {};
+        if (value) {
+            this.args[key] = String(value);
+            return true;
         }
-        return false
+        return false;
     }
-    setValue(value: string): void{
-        this.value = String(value)
+    setValue(value: string): void {
+        this.value = String(value);
     }
-    exec(): Promise<{stdout: string | Buffer,stderr: string | Buffer}>{
-        return new Promise((resolve,reject)=>{
-            execFile(this.name, this.getArgs(), {}, (err: Error | null, stdout: string | Buffer, stderr: string | Buffer)=>{
-                if(err) return reject(err)
-                resolve({
-                    stdout : stdout,
-                    stderr : stderr
-                })
-            })
-        })
+    exec(): Promise<{ stdout: string | Buffer; stderr: string | Buffer }> {
+        return new Promise((resolve, reject) => {
+            execFile(
+                this.name,
+                this.getArgs(),
+                {},
+                (
+                    err: Error | null,
+                    stdout: string | Buffer,
+                    stderr: string | Buffer
+                ) => {
+                    if (err) return reject(err);
+                    resolve({
+                        stdout: stdout,
+                        stderr: stderr,
+                    });
+                }
+            );
+        });
     }
-    getArgs(): string[]{
-        let result: Array<string> = []
-        if(this.args) {
-            Object.keys(this.args).forEach(key=>{
-                result.push('-'+key)
-                result.push(this.args![key])
-            })
+    getArgs(): string[] {
+        let result: Array<string> = [];
+        if (this.args) {
+            Object.keys(this.args).forEach((key) => {
+                result.push('-' + key);
+                result.push(this.args![key]);
+            });
         }
-        if(this.value) {
-            result.push(this.value)
+        if (this.value) {
+            result.push(this.value);
         }
-        return result
+        return result;
     }
+}
+
+export interface StdInput {
+    chunk: string | Buffer;
+    encoding: BufferEncoding;
 }
 
 export interface CommandsResult {
-    stdout: Readable
-    stderr: Readable
+    stdout: Readable;
+    stderr: Readable;
 }
 
-export function execCommands(commands: Command[]): CommandsResult{
-    const stderr = new Readable()
-    let processes:ChildProcessWithoutNullStreams[] = []
+export function execCommands(
+    commands: Command[],
+    stdin?: StdInput
+): CommandsResult {
+    const stderr = new Readable();
+    let processes: ChildProcessWithoutNullStreams[] = [];
     for (let i = 0; i < commands.length; i++) {
-        const command = commands[i]
-        processes[i] = spawn(command.name, command.getArgs())
+        const command = commands[i];
+        processes[i] = spawn(command.name, command.getArgs());
     }
+
+    if (stdin && processes.length > 0) {
+        processes[0].stdin.write(stdin.chunk, stdin.encoding);
+        processes[0].stdin.end();
+    }
+
     for (let i = 0; i < commands.length; i++) {
-        if(i+1 < commands.length){
-            processes[i].stdout.pipe(processes[i+1].stdin)
+        if (i + 1 < commands.length) {
+            processes[i].stdout.pipe(processes[i + 1].stdin);
         }
     }
     for (let i = 0; i < commands.length; i++) {
         processes[i].stderr.on('data', (data) => {
-            stderr.push(data)
-            console.error(Buffer.from(data, "binary").toString())
-        })
+            stderr.push(data);
+            console.error(Buffer.from(data, 'binary').toString());
+        });
     }
-    processes[commands.length - 1].stdout.on("close",()=>{
-        stderr.push(null)
-    })
+    processes[commands.length - 1].stdout.on('close', () => {
+        stderr.push(null);
+    });
     return {
         stdout: processes[commands.length - 1].stdout,
-        stderr: stderr
-    }
+        stderr: stderr,
+    };
 }
--- src/openjtalk.ts
+++ src/openjtalk.ts
@@ -1,111 +1,170 @@
-import path from "path"
-import fs from "fs"
-import { Command, CommandsResult, execCommands } from "./command"
-import { Readable } from "stream"
-const uuid = require('uuid-v4')
+import path from 'path';
+import fs from 'fs';
+import os from 'os';
+import { Command, CommandsResult, execCommands } from './command';
+import { Readable } from 'stream';
+import { encode } from 'iconv-lite';
+const uuid = require('uuid-v4');
 
-const OPENJTALK_PATH = path.join(__dirname, "../bin/open_jtalk")
+const platform = os.platform();
+const arch = os.arch();
 
-const config = JSON.parse(fs.readFileSync(path.join(__dirname, '../openjtalk.json'), 'utf8'))
+const encoding = (() => {
+    if (platform === 'win32') {
+        return 'Shift_JIS';
+    } else {
+        return 'utf8';
+    }
+})();
 
-export const DEFAULT_DICTIONARY_PATH = path.join(__dirname, '../dic/', config.dictionary)
-export const DEFAULT_SPEAKER_PATH = path.join(__dirname, '../voice/' + config.speaker.mei_normal + '.htsvoice')
+const OJT_COM = (() => {
+    if (platform === 'win32' && arch === 'x64') {
+        return path.join(__dirname, '../bin/open_jtalk.exe');
+    } else if (platform === 'linux' && arch === 'x64') {
+        return path.join(__dirname, '../bin/open_jtalk');
+    } else if (platform === 'darwin' && arch === 'x64') {
+        return path.join(__dirname, '../bin/open_jtalk');
+    } else {
+        throw new Error('Unsupported platform or architecture');
+    }
+})();
+
+const config = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../openjtalk.json'), 'utf8')
+);
+
+export const DEFAULT_DICTIONARY_PATH = (() => {
+    if (encoding === 'Shift_JIS') {
+        return path.join(__dirname, '../dic/', config.dictionary['shift_jis']);
+    } else {
+        return path.join(__dirname, '../dic/', config.dictionary['utf-8']);
+    }
+})();
+
+export const DEFAULT_SPEAKER_PATH = path.join(
+    __dirname,
+    '../voice/' + config.speaker.mei_normal + '.htsvoice'
+);
 
 export interface OJTVoiceOption {
-    speaker?: string      // -m
-    samplingRate?: number // -s
-    framePeriod?: number  // -p
-    formant?: number      // -a
-    postFilter?: number   // -b
-    speed?: number        // -r
-    pitch?: number        // -fm
-    uvThreshold?: number  // -u
-    spectrum?: number     // -jm
-    intonation?: number   // -jf
-    volume?: number       // -g
-    bufferSize?: number   // -z
+    speaker?: string; // -m
+    samplingRate?: number; // -s
+    framePeriod?: number; // -p
+    formant?: number; // -a
+    postFilter?: number; // -b
+    speed?: number; // -r
+    pitch?: number; // -fm
+    uvThreshold?: number; // -u
+    spectrum?: number; // -jm
+    intonation?: number; // -jf
+    volume?: number; // -g
+    bufferSize?: number; // -z
 }
 
 export interface OJTOutputOption {
-    path?: string,        // -ow
-    log?: string          // -ot
+    path?: string; // -ow
+    log?: string; // -ot
 }
 
 export interface OJTOption {
-    dictionary?: string    // -x
-    voice?: OJTVoiceOption,
-    output?: OJTOutputOption
+    dictionary?: string; // -x
+    voice?: OJTVoiceOption;
+    output?: OJTOutputOption;
 }
 
 interface OJTArgument {
-    x : string
-    m : string
-    ow? : string
-    ot? : string
-    s? : number
-    p? : number
-    a? : number
-    b? : number
-    r? : number
-    fm? : number
-    u? : number
-    jm? : number
-    jf? : number
-    g? : number
-    z? : number
+    x: string;
+    m: string;
+    ow?: string;
+    ot?: string;
+    s?: number;
+    p?: number;
+    a?: number;
+    b?: number;
+    r?: number;
+    fm?: number;
+    u?: number;
+    jm?: number;
+    jf?: number;
+    g?: number;
+    z?: number;
 }
 
-function createOJTArgs(option: OJTOption):  OJTArgument{
+function createOJTArgs(option: OJTOption): OJTArgument {
     return {
-         x: option.dictionary || DEFAULT_DICTIONARY_PATH,
-         m: option.voice?.speaker || DEFAULT_SPEAKER_PATH,
+        x: option.dictionary || DEFAULT_DICTIONARY_PATH,
+        m: option.voice?.speaker || DEFAULT_SPEAKER_PATH,
         ow: option.output?.path,
         ot: option.output?.log,
-         s: option.voice?.samplingRate,
-         p: option.voice?.framePeriod,
-         a: option.voice?.formant,
-         b: option.voice?.postFilter,
-         r: option.voice?.speed,
+        s: option.voice?.samplingRate,
+        p: option.voice?.framePeriod,
+        a: option.voice?.formant,
+        b: option.voice?.postFilter,
+        r: option.voice?.speed,
         fm: option.voice?.pitch,
-         u: option.voice?.uvThreshold,
+        u: option.voice?.uvThreshold,
         jm: option.voice?.spectrum,
         jf: option.voice?.intonation,
-         g: option.voice?.volume,
-         z: option.voice?.bufferSize
+        g: option.voice?.volume,
+        z: option.voice?.bufferSize,
+    };
+}
+
+function getOJTCommands(args: OJTArgument): Command {
+    const ojt = new Command(OJT_COM);
+    ojt.setArg('m', args.m);
+    ojt.setArg('x', args.x);
+    if (typeof args.ow !== 'undefined') {
+        ojt.setArg('ow', args.ow);
     }
+    if (typeof args.ot !== 'undefined') {
+        ojt.setArg('ot', args.ot);
+    }
+    if (typeof args.s !== 'undefined') {
+        ojt.setArg('s', args.s);
+    }
+    if (typeof args.p !== 'undefined') {
+        ojt.setArg('p', args.p);
+    }
+    if (typeof args.a !== 'undefined') {
+        ojt.setArg('a', args.a);
+    }
+    if (typeof args.b !== 'undefined') {
+        ojt.setArg('b', args.b);
+    }
+    if (typeof args.r !== 'undefined') {
+        ojt.setArg('r', args.r);
+    }
+    if (typeof args.fm !== 'undefined') {
+        ojt.setArg('fm', args.fm);
+    }
+    if (typeof args.u !== 'undefined') {
+        ojt.setArg('u', args.u);
+    }
+    if (typeof args.jm !== 'undefined') {
+        ojt.setArg('jm', args.jm);
+    }
+    if (typeof args.jf !== 'undefined') {
+        ojt.setArg('jf', args.jf);
+    }
+    if (typeof args.g !== 'undefined') {
+        ojt.setArg('g', args.g);
+    }
+    if (typeof args.z !== 'undefined') {
+        ojt.setArg('z', args.z);
+    }
+    return ojt;
 }
 
-function getOJTCommands(str:string, args: OJTArgument): Command[]{
-    const echo = new Command("echo")
-    echo.setValue(str)
-    const ojt = new Command(OPENJTALK_PATH)
-    ojt.setArg('m', args.m)
-    ojt.setArg('x', args.x)
-    ojt.setArg('ow', args.ow)
-    ojt.setArg('ot', args.ot)
-    ojt.setArg('s', args.s)
-    ojt.setArg('p', args.p)
-    ojt.setArg('a', args.a)
-    ojt.setArg('b', args.b)
-    ojt.setArg('r', args.r)
-    ojt.setArg('fm', args.fm)
-    ojt.setArg('u', args.u)
-    ojt.setArg('jm', args.jm)
-    ojt.setArg('jf', args.jf)
-    ojt.setArg('g', args.g)
-    ojt.setArg('z', args.z)
-    return [echo, ojt]
-}
-
-function getPlayCommand(): Command | null{
-	switch (process.platform) {
-		case "darwin":
-			return new Command('afplay')
-		case "linux":
-			return new Command('aplay')
-		default:
-			return null
-	}
+function getPlayCommand(): Command | null {
+    switch (process.platform) {
+        case 'darwin':
+            return new Command('afplay');
+        case 'linux':
+            return new Command('aplay');
+        default:
+            return null;
+    }
 }
 
 /**
@@ -113,9 +172,12 @@ function getPlayCommand(): Command | null{
  * @param name 話者の名前
  * @returns HTSボイスファイルのフルパス
  */
-export function getSpeaker(name: string): string | undefined{
-    if(!config.speaker[name]) return undefined
-    return path.join(__dirname, '../voice/' + config.speaker[name] + '.htsvoice')
+export function getSpeaker(name: string): string | undefined {
+    if (!config.speaker[name]) return undefined;
+    return path.join(
+        __dirname,
+        '../voice/' + config.speaker[name] + '.htsvoice'
+    );
 }
 
 /**
@@ -124,9 +186,13 @@ export function getSpeaker(name: string): string | undefined{
  * @param option 生成する音声の設定
  * @returns コンソールの実行結果
  */
-export function execute(text: string, option: OJTOption): CommandsResult{
-    const commands = getOJTCommands(text, createOJTArgs(option))
-    return execCommands(commands)
+export function execute(text: string, option: OJTOption): CommandsResult {
+    const command = getOJTCommands(createOJTArgs(option));
+    const encodedText = encode(text, encoding);
+    return execCommands([command], {
+        chunk: encodedText,
+        encoding: 'buffer' as BufferEncoding,
+    });
 }
 
 /**
@@ -135,13 +201,19 @@ export function execute(text: string, option: OJTOption): CommandsResult{
  * @param wavPath 音声ファイルの出力場所
  * @param voiceOption 生成する音声の設定
  */
-export async function outFile(text: string, wavPath: string, voiceOption?: OJTVoiceOption,): Promise<void>{
+export async function outFile(
+    text: string,
+    wavPath: string,
+    voiceOption?: OJTVoiceOption
+): Promise<void> {
     const option: OJTOption = {
         voice: voiceOption,
-        output: {path: wavPath}
-    }
-    const result = execute(text, option)
-    await new Promise<void>((resolve,_)=>result.stdout.on('close', ()=>resolve()))
+        output: { path: wavPath },
+    };
+    const result = execute(text, option);
+    await new Promise<void>((resolve, _) =>
+        result.stdout.on('close', () => resolve())
+    );
 }
 
 /**
@@ -150,9 +222,9 @@ export async function outFile(text: string, wavPath: string, voiceOption?: OJTVo
  * @param voiceOption 生成する音声の設定
  * @returns 音声データのストリーム
  */
-export function stream(text: string, voiceOption?: OJTVoiceOption): Readable{
-    const result = execute(text, {voice: voiceOption})
-    return result.stdout
+export function stream(text: string, voiceOption?: OJTVoiceOption): Readable {
+    const result = execute(text, { voice: voiceOption });
+    return result.stdout;
 }
 
 /**
@@ -161,17 +233,20 @@ export function stream(text: string, voiceOption?: OJTVoiceOption): Readable{
  * @param voiceOption 生成する音声の設定
  * @returns 音声のバイナリデータ
  */
-export function buffer(text: string, voiceOption?: OJTVoiceOption): Promise<Buffer>{
-    return new Promise<Buffer>((resolve, reject)=>{
+export function buffer(
+    text: string,
+    voiceOption?: OJTVoiceOption
+): Promise<Buffer> {
+    return new Promise<Buffer>((resolve, reject) => {
         try {
-            const stream = execute(text, {voice: voiceOption}).stdout
-            const data:Uint8Array[] = []
-            stream.on("data", chank=>data.push(chank))
-            stream.on("close", ()=>resolve(Buffer.concat(data)))
+            const stream = execute(text, { voice: voiceOption }).stdout;
+            const data: Uint8Array[] = [];
+            stream.on('data', (chank) => data.push(chank));
+            stream.on('close', () => resolve(Buffer.concat(data)));
         } catch (error) {
-            reject(error)
+            reject(error);
         }
-    })
+    });
 }
 
 /**
@@ -179,14 +254,17 @@ export function buffer(text: string, voiceOption?: OJTVoiceOption): Promise<Buff
  * @param text 生成する音声の原稿
  * @param voiceOption 生成する音声の設定
  */
-export async function talk(text: string, voiceOption?: OJTVoiceOption): Promise<void>{
-    const play = getPlayCommand()
-    if(!play) throw new Error("Playback on this OS is not supported.").stack
-    const tempDir = path.join(__dirname, '../temp')
-    if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir)
-    const wavPath = path.join(__dirname, '../temp/' + uuid() + '.wav')
-    await outFile(text, wavPath, voiceOption)
-    play.setValue(wavPath)
-    await play.exec()
-    fs.unlink(wavPath, ()=>{})
+export async function talk(
+    text: string,
+    voiceOption?: OJTVoiceOption
+): Promise<void> {
+    const play = getPlayCommand();
+    if (!play) throw new Error('Playback on this OS is not supported.').stack;
+    const tempDir = path.join(__dirname, '../temp');
+    if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir);
+    const wavPath = path.join(__dirname, '../temp/' + uuid() + '.wav');
+    await outFile(text, wavPath, voiceOption);
+    play.setValue(wavPath);
+    await play.exec();
+    fs.unlink(wavPath, () => {});
 }
